ba93970a053ddc89b76a2d76ac4e946f81c47152
github username <github email address>
2019-03-21 08:52:26 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
@@ -40,9 +42,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN characters
+  ON characters.species='human'
+  ORDER BY characters.species DESC
+  LIMIT 1"
 end
-
+# binding.pry
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
 end
